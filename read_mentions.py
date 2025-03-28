5f6118d0fabfcfb5cddebaeab0ddb1e50c921cdb
jayant yadav <jayantyadav202@gmail.com>
2025-03-28 10:29:29 +0100

    added download button and manual word entry textbox

--- read_mentions.py
+++ read_mentions.py
@@ -12,25 +12,48 @@ def _():
 
 @app.cell
 def _(mo):
-    import polars as pl
-    path_to_public = mo.notebook_location() / "public"
-    csv_files = path_to_public.glob("*.csv")
-    dataframes = {}
+    mention = mo.ui.text(value="innovation|Innovation", placeholder="innovation|Innovation")
+    mo.md(
+      f"""
+      Enter words sepearted by | 
 
-    for csv_file in csv_files:
+      {mention}
+      """
+    )
+    return (mention,)
+
+
+@app.cell
+def _(mention, mo):
+    import polars as pl
+    from pathlib import Path
+    path_to_public = mo.notebook_location() / "public"
+
+    print(path_to_public)
+
+    dataframes = {}
+    path_to_public = Path(path_to_public)
+    combined_df = pl.DataFrame()
+    for csv_file in path_to_public.iterdir():
+        print(csv_file)
         df = pl.read_csv(str(csv_file))
         # Extract the year from the file name
         year = csv_file.name.split()[-1].split(".")[0]
         # Add the year as a new column
         df = df.with_columns(pl.lit(year).alias("year"))
         # Filter the dataframe
-        filtered_df = df.filter(df["NarrativeText"].str.contains("innovation|Innovation"))
-        dataframes[csv_file.name] = filtered_df
+        filtered_df = df.filter(df["NarrativeText"].str.contains(mention.value))
+        combined_df = pl.concat([combined_df, filtered_df])
         print(filtered_df)
+        dataframes[csv_file.name] = filtered_df
 
+    combined_df = combined_df.rename({ "REGION_NAME": "region_name"
+        ,"BUSINESS_AREA_NAME": "country_name"})
+    combined_df = combined_df.drop("NarrativeTitle","NarrativeText")
     return (
+        Path,
+        combined_df,
         csv_file,
-        csv_files,
         dataframes,
         df,
         filtered_df,
@@ -41,7 +64,7 @@ def _(mo):
 
 
 @app.cell
-def _(dataframes):
+def _(dataframes, mo):
     import matplotlib.pyplot as plt
     country_counts = {}
     for file_name, df_ in dataframes.items():
@@ -61,9 +84,26 @@ def _(dataframes):
     for bar in bars:
         plt.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), str(bar.get_height()), 
                  ha='center', va='bottom', fontsize=10)
+    axis = plt.gca()
+    mo.md(
+        f"""
+        {mo.as_html(axis)}
+        """
+    )
+    return axis, bar, bars, country_counts, df_, file_name, plt
 
-    plt.show()
-    return country_counts, df_, file_name, plt
+
+@app.cell
+def _(combined_df, mo):
+    # CSV download using pandas
+    csv_download = mo.download(
+        data=combined_df.write_csv().encode("utf-8"),
+        filename="data.csv",
+        mimetype="text/csv",
+        label="Download CSV",
+    )
+    csv_download
+    return (csv_download,)
 
 
 if __name__ == "__main__":
